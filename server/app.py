12a69da9bc08d9033e99e8c95dcf3b1f47cd4d2a
kingsley <king808@my.yorku.ca>
2023-04-19 00:04:33 -0400

    Revert "add s3 to store pretrained model state"
    This reverts commit cd0f059998e340f4aa13b0296cd21631915ba826.

--- server/app.py
+++ server/app.py
@@ -10,7 +10,7 @@ import pandas as pd
 import torch
 
 
-from md_cell_predictor import initialize_model, predict
+from md_cell_predictor import MarkdownModel, predict
 
 
 app = Flask(__name__)
@@ -21,7 +21,19 @@ CORS(app)  # enable CORS for all routes
 app.config['UPLOAD_FOLDER'] = tempfile.gettempdir()
 
 # Model
-model = initialize_model()
+model = MarkdownModel()
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Load the state from the specified file
+model_path = "./input/ai4code-deberta-v3-base/model_best.pth"
+state = torch.load(model_path, map_location=torch.device('cpu'))
+
+# Update the model with the loaded state
+model.load_state_dict(state)
+
+# Move the model to the appropriate device
+model = model.to(device)
 
 
 def notebook_to_dataframe(path, notebook_id):
--- server/mySecrets.py
+++
@@ -1,3 +0,0 @@
-access_key = 'AKIATDHYONMPHHXLA7WO'
-secret_access_key = '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'
-bucket_ARN = 'arn:aws:s3:us-east-1:213121919774:accesspoint/cell-predictor-access-point'
